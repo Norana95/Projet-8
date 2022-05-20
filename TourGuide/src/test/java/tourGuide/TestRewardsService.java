a60ee5a3395dca139c693fc4cce044ca4dea3b84
Norana95 <boublenza.n@gmail.com>
2022-05-20 20:10:33 +0200

    fixe nearAllAttraction Test

--- TourGuide/src/test/java/tourGuide/TestRewardsService.java
+++ TourGuide/src/test/java/tourGuide/TestRewardsService.java
@@ -46,7 +46,6 @@ public class TestRewardsService {
 		assertTrue(rewardsService.isWithinAttractionProximity(attraction, attraction));
 	}
 	
-	@Ignore // Needs fixed - can throw ConcurrentModificationException
 	@Test
 	public void nearAllAttractions() {
 		GpsUtil gpsUtil = new GpsUtil();
